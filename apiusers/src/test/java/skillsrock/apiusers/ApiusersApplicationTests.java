282a1bef513d7dacefd2ba2941ac3186abf2f27b
SlavKoVrn <slavko.chita@gmail.com>
2025-07-17 14:23:17 +0300

    test init

--- apiusers/src/test/java/skillsrock/apiusers/ApiusersApplicationTests.java
+++ apiusers/src/test/java/skillsrock/apiusers/ApiusersApplicationTests.java
@@ -3,8 +3,26 @@ package skillsrock.apiusers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
 @SpringBootTest
 class ApiusersApplicationTests {
+
+    public ApiusersApplicationTests() {
+        RestAssured.baseURI = "http://localhost:8080";
+    }
+
+    @Test
+    void testGetAllUsers_ReturnsOkStatus() {
+        System.out.println("test begins");
+        given()
+            .when()
+                .get("/api/users")
+            .then()
+                .statusCode(200);
+    }
 
 	@Test
 	void contextLoads() {
